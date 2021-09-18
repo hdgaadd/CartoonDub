e8791af355b52134a78ee34d56180d307ddb2ae8
CodeChen <53108370+hdgaadd@users.noreply.github.com>
2021-09-19 01:32:56 +0800

    Delete

--- src/main/java/theme/Doraemon.java
+++
@@ -1,9 +0,0 @@
-package theme;
-
-import java.util.HashMap;
-//哆啦A梦主题
-public abstract class Doraemon extends Theme{
-    public Doraemon(String teamName, HashMap<String, String> map) {
-        super("Doraemon", teamName, map);
-    }
-}
