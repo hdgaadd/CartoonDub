47f4db034150e70162505c6d99bd166fd1f998a7
CodeChen <53108370+hdgaadd@users.noreply.github.com>
2021-09-19 01:33:26 +0800

    Update

---
+++ src/main/java/earth/CreateBeasts.java
@@ -0,0 +1,45 @@
+package earth;
+
+import javazoom.jl.player.Player;
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Random;
+
+public class CreateBeasts {
+    public Player player = null;
+    File directory = new File("");
+    String courseFile = directory.getCanonicalPath();
+    String str=courseFile+"\\CartoonDub\\src\\main\\java\\beastMusic\\";
+    public CreateBeasts() throws IOException {
+    }
+    //随机播放
+    public void random(String url){
+        File file = new File(str+url+"\\");//文件夹
+        File[] files = file.listFiles();
+        Random random = new Random();
+        int index=random.nextInt(files.length);
+        File mp3 = files[index];//mp3文件
+        music(mp3);
+    }
+    //变身
+    public void trans(String url){
+        music(new File(str+url+"\\trans.mp3"));
+    }
+    //laughter
+    public void laughter(String url){
+        music(new File(str+url+"\\laughter1.mp3"));
+    }
+    //播放
+    public void music(File mp3){
+        try {
+            new Player(new BufferedInputStream
+                    (new FileInputStream(
+                            mp3)))
+                    .play();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
